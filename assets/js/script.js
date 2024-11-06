7bb9b09425267377c175bec93380c80fa8161eb8
carlos <carloslospinos@gmail.com>
2024-11-06 11:47:22 +0100

    version1

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+function mostrarcaja1(){
+    const caja = document.getElementById("cajaoculta1");
+    caja.classList.toggle("oculto");
+}
+function mostrarcaja2(){
+    const caja = document.getElementById("cajaoculta2");
+    caja.classList.toggle("oculto");
+}
+function mostrarcaja3(){
+    const caja = document.getElementById("cajaoculta3");
+    caja.classList.toggle("oculto");
+}
+function ocultar(){
+    const caja = document.getElementById("cajaoculta");
+    caja.classList.toggle("oculto");
+}
